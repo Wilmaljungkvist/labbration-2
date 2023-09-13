53af390d8917945206b48f59807860776186a99a
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-09-13 12:09:19 +0200

    Add randomArrayItem function

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
-import { randomInt } from "./randomGenerator.js"
+import { randomInt, randomArrayItem } from "./randomGenerator.js"
 
 const randomInteger = randomInt(1, 10)
 console.log('Slumpat heltal:', randomInteger)
+
+const arr = ['Hej', 'Vad', 'Heter', 'du?', 'lol', 'jag', 'heter', 'wilma']
+const randomArray = randomArrayItem(arr)
+console.log('slumpad sträng i array:', randomArray)
--- src/js/randomGenerator.js
+++ src/js/randomGenerator.js
@@ -1,4 +1,10 @@
 export function randomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
     }
+
     
+export function randomArrayItem (Arr) {
+    const length = Arr.length
+    const randomIndex = randomInt(0, length - 1)
+    return Arr[randomIndex]
+}
