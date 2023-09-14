abbfc79de20c085eda5fcdd2cae04f7c67de37f5
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-09-14 13:57:18 +0200

    Add TODOs

--- src/js/randomGenerator.js
+++ src/js/randomGenerator.js
@@ -1,12 +1,16 @@
 export function randomInt (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+    // TODO: min måste vara mindre än max.
+    const randomInteger = Math.floor(Math.random() * (max - min + 1)) + min
+    return randomInteger
     }
 
 export function randomDecimal (min, max) {
+    // TODO: min måste vara mindre än max.
     return Math.random() * (max - min) + min
 }
     
 export function randomArrayIndex (Arr) {
+    // TODO: en Array kan inte vara tom eller om de skickas in annat än en array.
     const length = Arr.length
     const randomIndex = randomInt(0, length - 1)
     return randomIndex
@@ -22,6 +26,7 @@ export function randomBoolean () {
 }
 
 export function randomSequenceArray (min, max, length) {
+    // TODO: min måste vara mindre än max och längden kan inte vara 0.
     const arr = []
     for (let i = 0; i < length; i++) {
         arr.push(randomInt(min, max))
