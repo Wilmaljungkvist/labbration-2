05aeb7e1a7c129c3d948f33b9b8dc921f6bf66da
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-09-15 13:28:19 +0200

    Give information about generator

--- src/js/linearCongruentialGenerator.js
+++ src/js/linearCongruentialGenerator.js
@@ -1,3 +1,8 @@
 class LinearCongruentialGenerator {
-    
+    // TODO: Det kan inte vara vilka nummer som helst för då kommer generatorn börja repetera nummer efter ett tag. Uttrycket för denna generator är: Xn+1 = (a * Xn + c) % m. 
+    // Xn is the current pseudo-random number (seed).
+    // Xn+1 is the next pseudo-random number in the sequence.
+    // a is the multiplier.
+    // c is the increment.
+    // m is the modulus.
 }
